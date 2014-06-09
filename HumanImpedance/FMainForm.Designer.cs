68757afcee890d6e5fb2cab8d50e0872896e21a2
MrMuromec <ilia.d0@yandex.ru>
2014-06-09 19:00:15 +0400

    небольшие изменения
    убраны лишнии надписи

--- HumanImpedance/FMainForm.Designer.cs
+++ HumanImpedance/FMainForm.Designer.cs
@@ -33,11 +33,8 @@
       this.PathBox = new System.Windows.Forms.TextBox();
       this.PlotPanel = new System.Windows.Forms.PictureBox();
       this.ReadFileButton = new System.Windows.Forms.Button();
-      this.groupBox1 = new System.Windows.Forms.GroupBox();
       this.ApplyFilter = new System.Windows.Forms.Button();
-      this.label2 = new System.Windows.Forms.Label();
       ((System.ComponentModel.ISupportInitialize)(this.PlotPanel)).BeginInit();
-      this.groupBox1.SuspendLayout();
       this.SuspendLayout();
       // 
       // OpenFileButton
@@ -70,16 +67,16 @@
       // 
       this.PlotPanel.BackColor = System.Drawing.SystemColors.Window;
       this.PlotPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.PlotPanel.Location = new System.Drawing.Point(33, 173);
+      this.PlotPanel.Location = new System.Drawing.Point(15, 108);
       this.PlotPanel.Name = "PlotPanel";
-      this.PlotPanel.Size = new System.Drawing.Size(648, 229);
+      this.PlotPanel.Size = new System.Drawing.Size(666, 294);
       this.PlotPanel.TabIndex = 3;
       this.PlotPanel.TabStop = false;
       this.PlotPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.PlotPanel_Paint);
       // 
       // ReadFileButton
       // 
-      this.ReadFileButton.Location = new System.Drawing.Point(593, 40);
+      this.ReadFileButton.Location = new System.Drawing.Point(606, 40);
       this.ReadFileButton.Name = "ReadFileButton";
       this.ReadFileButton.Size = new System.Drawing.Size(75, 23);
       this.ReadFileButton.TabIndex = 4;
@@ -87,42 +84,22 @@
       this.ReadFileButton.UseVisualStyleBackColor = true;
       this.ReadFileButton.Click += new System.EventHandler(this.ReadFileButton_Click);
       // 
-      // groupBox1
-      // 
-      this.groupBox1.Controls.Add(this.label2);
-      this.groupBox1.Controls.Add(this.ApplyFilter);
-      this.groupBox1.Location = new System.Drawing.Point(438, 80);
-      this.groupBox1.Name = "groupBox1";
-      this.groupBox1.Size = new System.Drawing.Size(243, 87);
-      this.groupBox1.TabIndex = 7;
-      this.groupBox1.TabStop = false;
-      this.groupBox1.Text = "Секция Фильтров";
-      // 
       // ApplyFilter
       // 
-      this.ApplyFilter.Location = new System.Drawing.Point(112, 9);
+      this.ApplyFilter.Location = new System.Drawing.Point(15, 79);
       this.ApplyFilter.Name = "ApplyFilter";
       this.ApplyFilter.Size = new System.Drawing.Size(94, 23);
       this.ApplyFilter.TabIndex = 0;
-      this.ApplyFilter.Text = "Применить";
+      this.ApplyFilter.Text = "Отфилтровать";
       this.ApplyFilter.UseVisualStyleBackColor = true;
       this.ApplyFilter.Click += new System.EventHandler(this.ApplyFilter_Click);
-      // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(7, 19);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(67, 13);
-      this.label2.TabIndex = 1;
-      this.label2.Text = "Фильтр №1";
       // 
       // FMainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(693, 414);
-      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.ApplyFilter);
       this.Controls.Add(this.ReadFileButton);
       this.Controls.Add(this.PlotPanel);
       this.Controls.Add(this.PathBox);
@@ -131,8 +108,6 @@
       this.Name = "FMainForm";
       this.Text = "Form1";
       ((System.ComponentModel.ISupportInitialize)(this.PlotPanel)).EndInit();
-      this.groupBox1.ResumeLayout(false);
-      this.groupBox1.PerformLayout();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -145,8 +120,6 @@
         private System.Windows.Forms.TextBox PathBox;
         private System.Windows.Forms.PictureBox PlotPanel;
         private System.Windows.Forms.Button ReadFileButton;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button ApplyFilter;
     }
 }
